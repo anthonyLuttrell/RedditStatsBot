03093050be5f17c4ce567a33465fec43840a6415
Anthony (stranger) <luttrell321@gmail.com>
2023-10-27 16:21:03 -0500

    more

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import datetime
 import time
 import json
 import praw
+from filelock import FileLock
+
+total_posts = 0
+total_comments = 0
+start_seconds = 0
+end_seconds = 0
 
 reddit = praw.Reddit(
     client_id="KLDbru6psQuK3GSKc0jfKg",
@@ -11,52 +18,104 @@ reddit = praw.Reddit(
 )
 
 subreddit = reddit.subreddit("cooking")
+lockfile = "stats.json.lock"
+lock = FileLock(lockfile, timeout=1)
 
 
-def user_exists(user_id):
+def get_stats():
+
+    most_comments = []
+    with lock:
+        with open("stats.json", "r") as f:
+            obj = json.load(f)
+            for user in obj["users"]:
+                total_user_comments = 0
+                total_user_score = 0
+                for score in obj["users"][user]["commentScore"]:
+                    total_user_comments += 1
+                    total_user_score += score
+                most_comments.append([str(user), total_user_comments, total_user_score])
+    high_scores = most_comments.copy()
+    sorted(most_comments, key=lambda x: int(x[1]))
+    sorted(high_scores, key=lambda x: int(x[2]))
+    # FIXME the sorts are not working right
+    print("Most comments: " + most_comments[0][0])
+    print("High score: " + high_scores[0][0])
+
+
+def user_exists(user_id_to_check):
     found = False
-    file = open("stats.json", "r")
-    obj = json.load(file)
-    for user in obj["users"]:
-        if user_id == user:
-            found = True
-            break
+    with open("stats.json", "r") as f:
+        obj = json.load(f)
+        for user in obj["users"]:
+            if user_id_to_check == user:
+                found = True
+                break
     return found
 
 
+def update_existing(comment_to_update):
+    with lock:
+        with open("stats.json", "r+") as file_append_existing:
+            obj = json.load(file_append_existing)
+            users_obj = obj["users"][user_id]
+
+            try:
+                index = users_obj["commentId"].index(str(comment_to_update.id))
+            except ValueError:
+                index = -1
+
+            if index >= 0:
+                # if the comment already exists, update the score
+                new_score = comment_to_update.score
+                users_obj["commentScore"][index] = new_score
+            else:
+                # if the comment doesn't exist, add the ID and the score
+                users_obj["commentId"].append(str(comment_to_update.id))
+                users_obj["commentScore"].append(comment_to_update.score)
+
+            file_append_existing.seek(0)
+            json.dump(obj, file_append_existing, indent=2)
+
+
+def add_new(comment_to_add):
+    with lock:
+        with open("stats.json", "r+") as f:
+            new_data = json.load(f)
+            new_data["users"][str(comment_to_add.author)] = {"commentId": [comment_to_add.id],
+                                                             "commentScore": [comment_to_add.score]}
+        with lock:
+            with open("stats.json", "w") as f:
+                json.dump(new_data, f, indent=2)
+
+
 while True:
+    get_stats()
+    start_seconds = time.process_time()
+
     for submission in subreddit.hot(limit=1000):
+        total_posts += 1
+
         if submission.stickied is False:
+            print("Began scanning submission ID: " + str(submission.id) + " at " + time.strftime("%H:%M:%S"))
+
             for comment in submission.comments:
+                total_comments += 1
+
                 if hasattr(comment, "body"):
-                    user = str(comment.author)
-                    if user is not None:
-                        # fileWrite = open("stats.json", "a")
-                        # userData = json.load(fileWrite)
-                        if user_exists(user):
-                            print("User exists: " + user)
-                            with open("stats.json", "r+") as fileExAppend:
-                                userObj = json.load(fileExAppend)
-                                tempUserObj = userObj["users"][user]
-                                try:
-                                    index = tempUserObj["commentId"].index(str(comment.id))
-                                except ValueError:
-                                    index = -1
-                                if index >= 0:
-                                    # if the comment already exists, update the score
-                                    newScore = comment.score
-                                    tempUserObj["commentScore"][index] = newScore
-                                else:
-                                    # if the comment doesn't exist, add the ID and the score
-                                    tempUserObj["commentId"].append(str(comment.id))
-                                    tempUserObj["commentScore"].append(comment.score)
-                                fileExAppend.seek(0)
-                                json.dump(userObj, fileExAppend, indent=2)
+                    user_id = str(comment.author)
+
+                    if user_id != "None":
+
+                        if user_exists(user_id):
+                            update_existing(comment)
                         else:
-                            print("Adding new user: " + str(user))
-                            with open("stats.json", "r+") as fileAppend:
-                                newData = json.load(fileAppend)
-                                newData["users"][str(comment.author)] = {"commentId": [comment.id],
-                                                                         "commentScore": [comment.score]}
-                                fileAppend.seek(0)
-                                json.dump(newData, fileAppend, indent=2)
+                            add_new(comment)
+
+    end_seconds = time.process_time()
+    log = open("log.txt", "a")
+    log.write("\nTime of last loop:      " + str(datetime.timedelta(seconds=end_seconds - start_seconds)))
+    log.write("\nTotal posts scanned:    " + str(total_posts))
+    log.write("\nTotal comments scanned: " + str(total_comments))
+    log.close()
+    print("\n********** Main Loop Finished **********\n")

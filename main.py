aa5977e62ff4b5d8bb8d9ec032c6ec8894a9d89a
anthonyLuttrell <anthonyluttrell@gmail.com>
2023-11-18 23:13:48 -0600

    Began work on OOP structure

--- main.py
+++ main.py
@@ -1,56 +1,26 @@
-import argparse
 import datetime
 import json
 import math
 import os
-import praw
 import sys
 import time
-from praw.exceptions import RedditAPIException
-from prawcore import OAuthException
+from Scanner import Scanner
+from args import get_args
 
 # ************************************************* GLOBAL CONSTANTS ************************************************* #
-parser = argparse.ArgumentParser()
-parser.add_argument("-de",
-                    "--debug",
-                    help="Debug Mode, the parameter entered will be the subreddit that is used.")
-parser.add_argument("-d",
-                    "--day",
-                    help="Previous day will be the parameter entered.",
-                    choices=range(1, 32),
-                    default=0,
-                    type=int)
-args = parser.parse_args()
-
-if args.debug is None:
-    SUB = "Cooking"
-    NUM_OF_POSTS_TO_SCAN = 1000  # this will include stickied posts, which we are skipping
-    HOURS = 6
-    SLEEP_TIME_SECONDS = int(HOURS * 60 * 60)
-else:
-    SUB = args.debug
-    NUM_OF_POSTS_TO_SCAN = 10
-    SLEEP_TIME_SECONDS = 5
-
-BOT_NAME = "CookingStatsBot"
-REDDIT = praw.Reddit(BOT_NAME, user_agent="r/Cooking Stats Bot by u/96dpi")
-SUBREDDIT = REDDIT.subreddit(SUB)
 MONTHS = [['December', ''], ['January', ''], ['February', ''], ['March', ''], ['April', ''], ['May', ''], ['June', ''],
           ['July', ''], ['August', ''], ['September', ''], ['October', ''], ['November', ''], None]
+ARGS = get_args()
 NAME_IDX = 0
 FLAIR_TEMPLATE_ID_IDX = 1
 TOTAL_COMMENTS_IDX = 1
 AVG_SCORE_IDX = 1
 TOTAL_SCORE_IDX = 2
 NEG_COMMENTS_IDX = 3
-# ************************************************* GLOBAL VARIABLES ************************************************* #
-start_seconds = 0
-end_seconds = 0
-previous_day = args.day
-# ************************************************* GLOBAL FUNCTIONS ************************************************* #
+NUM_POSTS_TO_SCAN = ARGS.posts
 
 
-def edit_flair() -> bool:
+def edit_flair(obj, scanner) -> bool:
     """Deletes and sets user flair, then updates the wiki pages.
 
     We determine when it is a new month when the previous_day is greater than
@@ -64,26 +34,27 @@ def edit_flair() -> bool:
                 A bool that is true after the first iteration of the main loop
                 on the first day of the month.
     """
-    is_new_month = previous_day > int(datetime.datetime.today().day)
-    totals_arr = get_totals_array(obj)  # FIXME passing obj here is confusing
+    is_new_month = scanner.previous_day > int(datetime.datetime.today().day)
+    totals_arr = get_totals_array(obj)
     ratios_arr = get_ratios_array(totals_arr)
 
     if is_new_month:
         prev_month = int(datetime.datetime.today().month) - 1
-        set_flair_template_ids()
+        set_flair_template_ids(scanner.sub_instance)
 
-        if args.debug is None:
-            # this deletes the flair for all users of a subreddit!
-            SUBREDDIT.flair.delete_all()
+        # this deletes the flair for all users of a subreddit!
+        if ARGS.debug is None:
+            scanner.sub_instance.flair.delete_all()
             for i in range(0, len(ratios_arr)):
-                SUBREDDIT.flair.set(ratios_arr[i][NAME_IDX],
-                                    flair_template_id=MONTHS[prev_month][FLAIR_TEMPLATE_ID_IDX])
+                scanner.sub_instance.flair.set(ratios_arr[i][NAME_IDX],
+                                               flair_template_id=MONTHS[prev_month][FLAIR_TEMPLATE_ID_IDX])
         else:
+            print("Debug output only, no flair has been changed")
             for i in range(0, len(ratios_arr)):
-                print("Flair set for " + ratios_arr[i][NAME_IDX] + " with template " +
-                      str(MONTHS[prev_month][FLAIR_TEMPLATE_ID_IDX]))
+                print("Flair updated for: " + ratios_arr[i][NAME_IDX] + " for " +
+                      MONTHS[prev_month][FLAIR_TEMPLATE_ID_IDX])
 
-    edit_wiki(ratios_arr, is_new_month)
+    edit_wiki(ratios_arr, is_new_month, scanner)
     return is_new_month
 
 
@@ -144,14 +115,14 @@ def get_ratios_array(totals_arr) -> list:
     return ratio_arr
 
 
-def set_flair_template_ids():
+def set_flair_template_ids(sub_instance):
     missing_months = 0
 
     for month in MONTHS:
         if month is None:
             break
         found = False
-        for template in SUBREDDIT.flair.templates:
+        for template in sub_instance.flair.templates:
             if month[NAME_IDX] in template["text"]:
                 month[FLAIR_TEMPLATE_ID_IDX] = template["id"]
                 found = True
@@ -167,7 +138,7 @@ def set_flair_template_ids():
         print("User Flair template list is missing " + str(missing_months) + " month(s)")
 
 
-def edit_wiki(ratio_arr, new_month):
+def edit_wiki(ratio_arr, new_month, scanner):
     ####################################################################################################################
     # We edit the wiki upon two conditions:
     #
@@ -188,23 +159,25 @@ def edit_wiki(ratio_arr, new_month):
                              " [ average score: " + str(ratio_arr[i][1]) + " ]")
 
         # this will add a new revision to an existing page, or create the page if it doesn't exist
-        if args.debug:
-            pass  # SUBREDDIT.wiki[BOT_NAME].edit(content=wiki_content, reason=reason_string)
+        if ARGS.debug is None:
+            scanner.sub_instance.wiki[scanner.bot_name].edit(content=wiki_content, reason=reason_string)
         else:
-            print("Edit main wiki")
+            print("Wiki destination: " + scanner.bot_name)
+            print(wiki_content)
     else:
         wiki_content = "Last updated (UTC): " + str(datetime.datetime.utcnow())
         reason_string = "6-hour-update"
         for i in range(0, len(ratio_arr)):
             wiki_content += ("\n\n" + str(i + 1) + ". " + ratio_arr[i][0] +
                              " [ average score: " + str(ratio_arr[i][1]) + " ]")
-        if args.debug:
-            pass  # SUBREDDIT.wiki[BOT_NAME + "/" + reason_string].edit(content=wiki_content, reason=reason_string)
+        if ARGS.debug is None:
+            scanner.sub_instance.wiki[scanner.bot_name + "/" + reason_string].edit(content=wiki_content, reason=reason_string)
         else:
-            print("Edit 6-hour-update wiki")
+            print("Wiki destination: " + scanner.bot_name + "/" + reason_string)
+            print(wiki_content)
 
 
-def user_exists(user_id_to_check):
+def user_exists(obj, user_id_to_check):
     found = False
     for user in obj["users"]:
         if user_id_to_check == user:
@@ -213,7 +186,7 @@ def user_exists(user_id_to_check):
     return found
 
 
-def update_existing(comment_to_update):
+def update_existing(obj, comment_to_update, user_id: str):
     users_obj = obj["users"][user_id]
     id_arr = users_obj["commentId"]
     score_arr = users_obj["commentScore"]
@@ -232,20 +205,20 @@ def update_existing(comment_to_update):
         score_arr.append(comment_to_update.score)
 
 
-def add_new(comment_to_add):
+def add_new(obj, comment_to_add):
     obj["users"][str(comment_to_add.author)] = {"commentId": [comment_to_add.id],
                                                 "commentScore": [comment_to_add.score]}
 
 
-def sleep():
+def sleep(scanner):
     sleep_time = time.strftime("%H:%M:%S")
     # including this as a workaround for the Docker logs, remove when fixed
-    print("Sleeping since " + sleep_time + ", waking up in " + str(HOURS) + " hours.")
+    # print("Sleeping since " + sleep_time + ", waking up in " + str(scanner.get_time_slice()) + " hours.")
 
-    for i in range(0, SLEEP_TIME_SECONDS):
+    for i in range(0, scanner.sleep_seconds):
         # FIXME this does not print in Docker logs
-        # print("\r", "Sleeping since " + sleep_time + ", waking up in " +
-        #       str(SLEEP_TIME_SECONDS - i).rjust(5, "0") + " sec", end="")
+        print("\r", "Sleeping since " + sleep_time + ", waking up in " +
+              str(scanner.sleep_seconds - i).rjust(5, "0") + " sec", end="")
         time.sleep(1)
 
 
@@ -256,87 +229,104 @@ def sys_exit():
         os.system(exit(130))
 
 
-# **************************************************** MAIN LOOP ***************************************************** #
+def create_file(file_name):
+    # create the files if they don't exist
+    print()
+    with open(file_name, "a") as f:
+        f.seek(0)
+        if f.newlines is None:
+            # FIXME this is wrong, newlines does not detect if the file is empty.
+            print(file_name + " was created")
+            f.write("{\"users\":{}}")
+        else:
+            print(file_name + " already exists")
 
-try:
+
+def main():
+    scanner_queue = []
+    testing_scanner = Scanner("alphabetbot",
+                              "CookingStatsBot",
+                              ARGS.posts,
+                              10)
+    cooking_scanner = Scanner("Cooking",
+                              "CookingStatsBot",
+                              ARGS.posts,
+                              10)
+    scanner_queue.append(testing_scanner)
+    scanner_queue.append(cooking_scanner)
+
+    if ARGS.debug:
+        print("Debug flag is set!")
+
     try:
-        print("Logged in as:", REDDIT.user.me())
-    except OAuthException:
-        print("Unable to log in! Verify the credentials in the praw.ini file.")
-        sys_exit()
+        while True:
+            start_seconds = 0
+            end_seconds = 0
+            time_elapsed = 0
+            total_posts = 0
+            total_comments = 0
 
-    try:
-        SUBREDDIT.mod.accept_invite()
-        print("Mod invite accepted from r/" + SUB + ", starting main program.")
-    except RedditAPIException:
-        print("No pending mod invites from r/" + SUB + ". Assuming the account u/" + BOT_NAME + " is already a mod with"
-                                                                                                " flair and wiki permissions, starting main program.")
+            for scanner in scanner_queue:
+                file_name = scanner.sub_name + ".json"
+                create_file(file_name)
+                with open(scanner.sub_name + ".json", "r+") as f:
+                    obj = json.load(f)
+                    start_seconds = time.perf_counter()
 
-    total_comments = 0
-    last_total_comments = 0
+                    for submission in scanner.sub_instance.hot(limit=scanner.num_posts_to_scan):
 
-    while True:
-        last_total_comments = total_comments
-        time_elapsed = 0
-        total_posts = 0
-        total_comments = 0
+                        if submission.stickied is False:
+                            total_posts += 1
+                            # FIXME this does not print in Docker logs
+                            print("\r", "Began scanning submission ID " +
+                                  str(submission.id) + " at " + time.strftime("%H:%M:%S"), end="")
 
+                            submission.comments.replace_more(limit=0)
+                            for comment in submission.comments:
+                                if not comment.distinguished:
+                                    user_id = str(comment.author)
+                                    total_comments += 1
+                                    # FIXME this does not print in Docker logs
+                                    print("\r", "Total Comments Scanned: " + str(total_comments), end="")
+                                    if user_id != "None":
+                                        if user_exists(obj, user_id):
+                                            update_existing(obj, comment, user_id)
+                                        else:
+                                            add_new(obj, comment)
+
+                                time.sleep(0.1)  # avoids HTTP 429 errors
+                        time.sleep(0.1)  # avoids HTTP 429 errors
+
+                end_seconds = time.perf_counter()
+                time_elapsed += (end_seconds - start_seconds) / 60
+                print("\nTime elapsed: " + str(datetime.timedelta(minutes=time_elapsed)))
+
+                if edit_flair(obj, scanner):
+                    # clear out the comment log at the beginning of each month
+                    obj["users"] = {}
+
+                try:
+                    with open(scanner.sub_name + ".json", "w") as f:
+                        f.seek(0)
+                        json.dump(obj, f, indent=2)
+                    sleep(scanner)
+                    scanner.previous_day = datetime.datetime.today().day
+                except FileNotFoundError:
+                    print("File Not Found, exiting.")
+                    sys_exit()
+
+    except KeyboardInterrupt:
+        # catches Ctrl+C and IDE program interruption to ensure we write to the json file
         try:
-            with open("stats.json", "r+") as f:
-                obj = json.load(f)
-                start_seconds = time.perf_counter()
-
-                for submission in SUBREDDIT.hot(limit=NUM_OF_POSTS_TO_SCAN):
-
-                    if submission.stickied is False:
-                        total_posts += 1
-                        # FIXME this does not print in Docker logs
-                        print("\r", "Began scanning submission ID " +
-                              str(submission.id) + " at " + time.strftime("%H:%M:%S"), end="")
-
-                        submission.comments.replace_more(limit=0)
-                        for comment in submission.comments:
-                            if not comment.distinguished:
-                                user_id = str(comment.author)
-                                total_comments += 1
-                                # print("\r", "Total Comments Scanned: " + str(total_comments), end="")
-                                if user_id != "None":
-                                    if user_exists(user_id):
-                                        update_existing(comment)
-                                    else:
-                                        add_new(comment)
-
-                            time.sleep(0.1)  # avoids HTTP 429 errors
-                    time.sleep(0.1)  # avoids HTTP 429 errors
-
-            end_seconds = time.perf_counter()
-            time_elapsed += (end_seconds - start_seconds) / 60
-            print("\nTime elapsed: " + str(datetime.timedelta(minutes=time_elapsed)))
-
-            if edit_flair():
-                # clear out the comment log at the beginning of each month
-                obj["users"] = {}
-
-            try:
-                with open("stats.json", "w") as f:
+            print("\n-- Process halted, dumping JSON file --")
+            for scanner in scanner_queue:
+                with open(scanner.sub_name + ".json", "w") as f:
                     f.seek(0)
                     json.dump(obj, f, indent=2)
-                sleep()
-                previous_day = datetime.datetime.today().day
-            except FileNotFoundError:
-                print("File Not Found, exiting.")
-                sys_exit()
-        except FileNotFoundError:
-            print("File Not Found, exiting.")
+        except NameError:
             sys_exit()
-
-except KeyboardInterrupt:
-    # catches Ctrl+C and IDE program interruption to ensure we write to the json file
-    try:
-        print("\n-- Process halted, dumping JSON file --")
-        with open("stats.json", "w") as f:
-            f.seek(0)
-            json.dump(obj, f, indent=2)
-    except NameError:
         sys_exit()
-    sys_exit()
+
+
+if __name__ == "__main__":
+    main()
